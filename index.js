ffcd0e9dbe552bc7f4313c524ba7419916206e41
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-04-09 00:47:21 -0400

    ADD: Multiple call apis

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 /**
  * @author SteiNNx
- * @version 0.0.1-Beta 
+ * @version 0.0.2-Beta 
  * @description WebScrapping Sample Random
  */
 const axios = require('axios');
@@ -11,33 +11,39 @@ const fs = require('fs');
 
 const PORT = 8080;
 const app = express();
-const URL = 'https://production-na01-cencosud.demandware.net/s/Paris/jugueteria/api+cat+dodo+funko+kd/?start=0&sz=40';
-
+const URL_1 = 'https://production-na01-cencosud.demandware.net/s/Paris/jugueteria/api+cat+dodo+funko+kd/?start=0&sz=40';
+const URL_2 = 'https://production-na01-cencosud.demandware.net/s/Paris/jugueteria/api+cat+dodo+funko+kd/?start=40&sz=40';
 /**
  * 
  * @param {string} url url del sitio de prod de paris xddd
  * @param {string} pathNameFile ruta destino json con datos
  */
-const getDataToJsonFromUrl = (url, pathNameFile) => {
-    axios(url)
+const getDataToJsonFromUrl = async (url, pathNameFile) => {
+    await axios(url)
         .then((result) => {
             const html = result.data;
             const $ = cheerio.load(html);
             const list_funkopop = [];
 
+            console.log("|-----------------------------------|");
+            console.log("|------------ Iniciado -------------|");
+            console.log("|-----------------------------------|");
+
             $('.js-product-position', html).each(function (i) {
                 let title = $(this).find('.ellipsis_text:first').text();
                 let image_url = $(this).find('img:nth-child(2)').attr('data-src');
 
-                console.log(image_url);
                 list_funkopop[i] = {
                     title,
                     image_url,
                 };
+                console.log(`Migrated: ${i} ${title}`)
             });
 
             fs.writeFileSync(pathNameFile, JSON.stringify(list_funkopop, null, 4));
-
+            console.log("|-----------------------------------|");
+            console.log("|-------- Proceso Terminado --------|");
+            console.log("|-----------------------------------|");
         }).catch((err) => {
             console.log(err);
         });
@@ -46,7 +52,13 @@ const getDataToJsonFromUrl = (url, pathNameFile) => {
 /**
  * Ejecucion Codigo
  */
-getDataToJsonFromUrl(URL, "./result.json");
+const startMigrate = async () => {
+    await getDataToJsonFromUrl(URL_1, "./api/result.json");
+    await getDataToJsonFromUrl(URL_2, "./api/result2.json");
+    process.exit(1)
+}
+
+startMigrate();
 
 /**
  * Listener Server
