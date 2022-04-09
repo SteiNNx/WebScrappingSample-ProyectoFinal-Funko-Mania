f462ee46df8b4ddd33ee01fd7734614eb58a38e4
Jorge Reyes <jorge_irving_15@hotmail.com>
2022-04-09 01:10:02 -0400

    ADD: api, se actualiza script webscrapping

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ const PORT = 8080;
 const app = express();
 const URL_1 = 'https://production-na01-cencosud.demandware.net/s/Paris/jugueteria/api+cat+dodo+funko+kd/?start=0&sz=40';
 const URL_2 = 'https://production-na01-cencosud.demandware.net/s/Paris/jugueteria/api+cat+dodo+funko+kd/?start=40&sz=40';
+
+
+/**
+ * 
+ * @param {string} stringPrice 
+ * @returns {string}
+ */
+const cleanPrice = stringPrice => {
+    return stringPrice
+        .replace(".", "")
+        .replace("$", "");
+}
+
 /**
  * 
  * @param {string} url url del sitio de prod de paris xddd
@@ -30,12 +43,25 @@ const getDataToJsonFromUrl = async (url, pathNameFile) => {
             console.log("|-----------------------------------|");
 
             $('.js-product-position', html).each(function (i) {
-                let title = $(this).find('.ellipsis_text:first').text();
-                let image_url = $(this).find('img:nth-child(2)').attr('data-src');
+                let title = $(this)
+                    .find('.ellipsis_text:first')
+                    .text();
+
+                let image_url = $(this)
+                    .find('img:nth-child(2)')
+                    .attr('data-src');
+
+                let price = $(this)
+                    .find('.price__text:first')
+                    .text()
+                    .trim();
+
+                price = cleanPrice(price);
 
                 list_funkopop[i] = {
                     title,
                     image_url,
+                    price,
                 };
                 console.log(`Migrated: ${i} ${title}`)
             });
